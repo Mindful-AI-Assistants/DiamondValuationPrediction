b0653b8c9d786f807a6f9c21cabacce2eb6862c6
Fabiana Campanari <fabicampanari@gmail.com>
2024-08-07 12:21:47 -0300

    Update post-create.sh
    Signed-off-by: Fabiana Campanari <fabicampanari@gmail.com>

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,10 +1,13 @@
-#!/bin/bash
+#!/usr/bin/env bash
+gh release download --repo dependabot/cli -p "*linux-amd64.tar.gz"
+tar xzvf ./*.tar.gz >/dev/null 2>&1
+sudo mv dependabot /usr/local/bin
+rm ./*.tar.gz
 
-set -e
+# The image comes loaded with 8.0 preview SDK, but we need a stable 7.0 runtime for running tests
+sudo wget https://dot.net/v1/dotnet-install.sh
+sudo chmod +x dotnet-install.sh
+sudo ./dotnet-install.sh -c 7.0 --runtime dotnet --install-dir /usr/local/dotnet/current
+sudo rm ./dotnet-install.sh
 
-if [[ "${CODESPACES}" == true ]]; then
-  echo "Fixing permissions of /tmp for GitHub Codespaces..." >&2
-  sudo chmod 1777 /tmp
-fi
-
-pnpm install
+echo "export LOCAL_GITHUB_ACCESS_TOKEN=$GITHUB_TOKEN" >> ~/.bashrc
